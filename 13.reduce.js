0a4eb53cf4031236142d0500fb6a3074360ffc52
David Vera <dsvera.a@gmail.com>
2024-05-28 01:41:54 -0400

    quité una coma

--- 13.reduce.js
+++ 13.reduce.js
@@ -1,6 +1,6 @@
 const numbers = [1, 2, 3]
 
-const sum = numbers.reduce((accumulator, current,) => accumulator + current, 0)
+const sum = numbers.reduce((accumulator, current) => accumulator + current, 0)
 
 console.log(sum)
 
